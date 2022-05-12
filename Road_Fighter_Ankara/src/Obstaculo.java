37d43c179ba0c70a22a2eeb6a8ab67624d2aaaa7
alejandroruiz9 <alejandroruiz43@gmail.com>
2022-05-12 19:55:14 -0300

    Road Fighter 1.2
    Falta terminar :
    *clase Mapa
    *metodo de explotar de auto
    *crear clase ComponenteDeMapa que englobara Automovil y Obstaculo
    *modificar metodo chocar para que choque con ComponenteDeMapa

--- Road_Fighter_Ankara/src/Obstaculo.java
+++ Road_Fighter_Ankara/src/Obstaculo.java
@@ -1,4 +1,11 @@
 
 public class Obstaculo {
+	private Punto posicion;
 
+	public Obstaculo(Punto posicion) {
+		super();
+		this.posicion = posicion;
+	}
+	
+	
 }
--- Road_Fighter_Ankara/src/Test.java
+++ Road_Fighter_Ankara/src/Test.java
@@ -1,5 +1,5 @@
 
-import org.junit.Assert;
+
 
 import junit.framework.TestCase;
 
